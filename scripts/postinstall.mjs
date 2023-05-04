299bc66fb1d5990f795a71c5cddda22ec66296aa
李瑞丰 <liruifeng1024@gmail.com>
2023-05-04 11:45:44 +0800

    chore: fix ci (#1)
    * chore: fix ci
    
    * chore: fix ci
    
    * Update ci.yml
    
    * fix ci scripts
    
    * fix rust ci
    
    * fix rust ci
    
    * fix rust ci
    
    * fix rust ci
    
    * fix rust ci
    
    * fix rust ci
    
    * fix rust ci
    
    * fix rust ci
    
    * fix rust ci

---
+++ scripts/postinstall.mjs
@@ -0,0 +1,10 @@
+import { mkdir } from 'node:fs/promises'
+import { fileURLToPath } from 'node:url'
+import { resolve } from 'node:path'
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+// tauri 要求有 dist 文件夹
+try {
+  await mkdir(resolve(__dirname, '../dist'))
+} catch {
+  // don't care
+}
